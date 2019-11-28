648f3d15b8c15e8f4c7fbba404784391bc88bc46
jigyasha-exalogic <jigyasha@exalogic.in>
2019-11-28 18:45:45 +0530

    6 commit

--- db/migrate/20191125103841_add_fname_to_users.rb
+++ db/migrate/20191125103841_add_fname_to_users.rb
@@ -2,5 +2,9 @@ class AddFnameToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :fname, :string
     add_column :users, :lname, :string
+    add_column :users, :phonenumber, :string
+    add_column :users, :emergency_number, :string
+    add_column :users, :pincode, :string
+
   end
 end
